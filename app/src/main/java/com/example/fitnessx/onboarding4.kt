2e1403af88218a69ebf162d6f002bf3e48e545a5
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-11-08 18:16:34 +0530

    03

--- app/src/main/java/com/example/fitnessx/onboarding4.kt
+++ app/src/main/java/com/example/fitnessx/onboarding4.kt
@@ -1,27 +1,20 @@
 package com.example.fitnessx
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class onboarding3 : AppCompatActivity() {
+class onboarding4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding3)
+        setContentView(R.layout.activity_onboarding4)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val button = findViewById<ImageView>(R.id.imageView7)
-        button.setOnClickListener {
-            val intent = Intent(this, onboarding4::class.java)
-            startActivity(intent)
         }
     }
 }
