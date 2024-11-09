0164086c83271c8b47e7ed9c4f735ef3d5d370e3
Makthala Sai Teja Goud <mstg1102@gmail.com>
2024-11-09 08:34:02 +0530

    05

--- app/src/main/java/com/example/fitnessx/LoginPage.kt
+++ app/src/main/java/com/example/fitnessx/LoginPage.kt
@@ -3,38 +3,31 @@ package com.example.fitnessx
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.*
 
-class RegisterPage1 : AppCompatActivity() {
+class LoginPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_page1)
+        setContentView(R.layout.activity_login_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val checkBox = findViewById<CheckBox>(R.id.checkBox)
-        val button2 = findViewById<Button>(R.id.button2)
-        button2.isEnabled = false
-        checkBox.setOnCheckedChangeListener { _, isChecked ->
-            if(checkBox.isChecked){
-                button2.isEnabled = true
-            }
-            else{
-                button2.isEnabled = false
-                Toast.makeText(this, "Please read terms and conditions", Toast.LENGTH_SHORT).show()
-            }
-        }
         val textView7 = findViewById<TextView>(R.id.textView7)
         textView7.setOnClickListener{
-            val intent = Intent(this,LoginPage::class.java)
+            val intent = Intent(this,RegisterPage1::class.java)
+            startActivity(intent)
+        }
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener{
+            val intent = Intent(this,SuccessRegistartion::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/fitnessx/SuccessRegistartion.kt
+++ app/src/main/java/com/example/fitnessx/SuccessRegistartion.kt
@@ -1,28 +1,20 @@
 package com.example.fitnessx
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class onboarding4 : AppCompatActivity() {
+class SuccessRegistartion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding4)
+        setContentView(R.layout.activity_success_registartion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button = findViewById<ImageView>(R.id.imageView9)
-        button.setOnClickListener {
-            val intent = Intent(this, RegisterPage1::class.java)
-            startActivity(intent)
         }
     }
 }
